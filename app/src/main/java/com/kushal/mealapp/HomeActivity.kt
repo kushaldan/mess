da6d068cd079dc736d56dbfb6ddc451c72a11ae7
tax2791 <tax2791@gmail.com>
2025-01-25 01:13:19 +0530

    database jetpack compose a separate folder has been created. no error found but display has not been implemented yet

--- app/src/main/java/com/kushal/mealapp/HomeActivity.kt
+++ app/src/main/java/com/kushal/mealapp/HomeActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Button
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
+
 class HomeActivity : AppCompatActivity() {
 
     @SuppressLint("SetJavaScriptEnabled")
--- app/src/main/java/database/AppDatabase.kt
+++ app/src/main/java/database/AppDatabase.kt
@@ -6,4 +6,5 @@ import androidx.room.RoomDatabase
 @Database(entities = [Meal::class, Deposit::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun mealDao(): MealDao
+
 }
